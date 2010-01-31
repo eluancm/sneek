fdceada8d57e389cb0c1a874cecd97c0fea25718
admin <admin@det1re.de>
2010-01-31 01:09:30 +0000

    boot2 compiling batch changed to work with .app file, too

--- build_boot2.cmd
+++ build_boot2.cmd
@@ -6,7 +6,17 @@ cd ..\fs
 make clean
 make
 cd ..
+if exist 0000000E (goto wo_app) else (goto w_app)
+
+:wo_app
 elfins\elfins fs\iosmodule.elf 0000000E 0000000E-tmp
+goto esmodule
+
+:w_app
+elfins\elfins fs\iosmodule.elf 0000000e.app 0000000E-tmp
+goto esmodule
+
+:esmodule
 elfins\elfins es\esmodule.elf 0000000E-tmp 0000000E-tm
 boot2me\boot2me 0000000E-tm boot2.bin
 del 0000000E-tmp
