bc8fcc0a29f853dedf88621bd22453b5ae508a21
eggdog <eggdog@fdb4e6e6-0440-11df-9d25-5dd9327a4d76>
2010-03-14 21:24:18 +0000

    - Added elfins-v1 (linux) & elfins-v1.exe (windows), as these are needed to create a working boot2.bin without the di module.
    - build_boot2_di.cmd will build a boot2.bin and di.bin providing you have the correct .app files in the sneek folder

--- build_boot2.cmd
+++ build_boot2.cmd
@@ -9,15 +9,15 @@ cd ..
 if exist 0000000E (goto wo_app) else (goto w_app)
 
 :wo_app
-elfins\elfins fs\iosmodule.elf 0000000E 0000000E-tmp
+elfins\elfins-v1 fs\iosmodule.elf 0000000E 0000000E-tmp
 goto esmodule
 
 :w_app
-elfins\elfins fs\iosmodule.elf 0000000e.app 0000000E-tmp
+elfins\elfins-v1 fs\iosmodule.elf 0000000e.app 0000000E-tmp
 goto esmodule
 
 :esmodule
-elfins\elfins es\esmodule.elf 0000000E-tmp 0000000E-tm
+elfins\elfins-v1 es\esmodule.elf 0000000E-tmp 0000000E-tm
 boot2me\boot2me 0000000E-tm boot2.bin
 del 0000000E-tmp
 del 0000000E-tm
