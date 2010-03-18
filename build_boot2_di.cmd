d492096c8c26f94d2c7a199ddd7ecf0da89a6756
eggdog <eggdog@fdb4e6e6-0440-11df-9d25-5dd9327a4d76>
2010-03-18 23:55:34 +0000

    - Removed old files - Updated build_boot2_di.cmd

--- build_boot2_di.cmd
+++ build_boot2_di.cmd
@@ -5,30 +5,16 @@ make
 cd ..\fs
 make clean
 make
-cd ..\di
+cd ..\fs-usb
 make clean
 make
+cd ..\di
+make
+make clean
 cd ..
-if exist 0000000E (goto wo_app) else (goto w_app)
-
-:wo_app
-elfins\elfins fs\iosmodule.elf 0000000E 0000000E-tmp
-goto esmodule
-
-:w_app
-elfins\elfins fs\iosmodule.elf 0000000e.app 0000000E-tmp
-goto esmodule
-
-:esmodule
-elfins\elfins es\esmodule.elf 0000000E-tmp 0000000E-tm
-boot2me\boot2me 0000000E-tm boot2.bin
+elfins\elfins fs\iosmodule.elf 0000000E 0000000E-tmp -s
+elfins\elfins es\esmodule.elf 0000000E-tmp boot2_sd.bin
+elfins\elfins fs-usb\iosmodule.elf 0000000E 0000000E-tmp -u
+elfins\elfins es\esmodule.elf 0000000E-tmp boot2_usb.bin
+elfins\elfins fs-usb\iosmodule.elf 00000001 di.bin -d
 del 0000000E-tmp
-del 0000000E-tm
-
-if exist 00000001 (goto wo_app_di) else (goto w_app_di)
-
-:wo_app_di
-elfins\elfins di\iosmodule.elf 00000001 di.bin
-
-:w_app_di
-elfins\elfins di\iosmodule.elf 00000001.app di.bin
