ae352c4f8800ba4bad046e2ea728fa3100a1d2f0
eggdog <eggdog@fdb4e6e6-0440-11df-9d25-5dd9327a4d76>
2010-03-21 16:02:14 +0000

    *sigh* Linux users :p

--- build_boot2.cmd
+++ build_boot2.cmd
@@ -10,18 +10,18 @@ echo Patching..
 
 echo Patching for SNEEK
 echo IOSKPatch: SD (with di) 
-IOSKpatch\IOSKPatch 0000000E.app 0000000E-TMP.app -s -d > NUL
+IOSKpatch\IOSKPatch 0000000e.app 0000000E-TMP.app -s -d > NUL
 echo elfins: Creating boot2_di.bin (SDCard as NAND, with DI module support)
 ELFIns\elfins 0000000E-TMP boot2_di.bin es\esmodule.elf fs\iosmodule.elf > NUL
 
 echo IOSKPatch: SD (no di) 
-IOSKpatch\IOSKPatch 0000000E.app 0000000E-TMP.app -s > NUL
+IOSKpatch\IOSKPatch 0000000e.app 0000000E-TMP.app -s > NUL
 echo elfins: Creating boot2_sd.bin (SDCard as NAND)
 ELFIns\elfins 0000000E-TMP boot2_sd.bin es\esmodule.elf fs\iosmodule.elf > NUL
 
 echo Patching for UNEEK
 echo IOSKPatch: USB (no di) 
-IOSKpatch\IOSKPatch 0000000E.app 0000000E-TMP.app -u > NUL
+IOSKpatch\IOSKPatch 0000000e.app 0000000E-TMP.app -u > NUL
 echo elfins: Creating boot2_usb.bin (USB as NAND)
 ELFIns\elfins 0000000E-TMP.app boot2_usb.bin es\esmodule.elf fs-usb\iosmodule.elf > NUL
 
--- build_boot2.sh
+++ build_boot2.sh
@@ -9,18 +9,18 @@ echo Patching..
 
 echo Patching for SNEEK
 echo IOSKPatch: SD \(with di\) 
-./IOSKpatch/IOSKPatch 0000000E 0000000E-TMP -s -d > /dev/null
+./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP -s -d > /dev/null
 echo elfins: Creating boot2_di.bin \(SDCard as NAND, with DI module support\)
 ./ELFIns/elfins 0000000E-TMP boot2_di.bin es/esmodule.elf fs/iosmodule.elf > /dev/null
 
 echo IOSKPatch: SD \(no di\) 
-./IOSKpatch/IOSKPatch 0000000E 0000000E-TMP -s > /dev/null
+./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP -s > /dev/null
 echo elfins: Creating boot2_sd.bin \(SDCard as NAND\)
 ./ELFIns/elfins 0000000E-TMP boot2_sd.bin es/esmodule.elf fs/iosmodule.elf > /dev/null
 
 echo Patching for UNEEK
 echo IOSKPatch: USB \(no di\)
-./IOSKpatch/IOSKPatch 0000000E 0000000E-TMP -u > /dev/null
+./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP -u > /dev/null
 echo elfins: Creating boot2_usb.bin \(USB as NAND\)
 ./ELFIns/elfins 0000000E-TMP boot2_usb.bin es/esmodule.elf fs-usb/iosmodule.elf > /dev/null
 
