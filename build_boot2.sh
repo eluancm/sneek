e4a3362cb13b592a5d60e400ab970d8f0a0d2f22
eggdog <eggdog@fdb4e6e6-0440-11df-9d25-5dd9327a4d76>
2010-03-21 16:10:05 +0000

    I should check my work :)

--- build_boot2.sh
+++ build_boot2.sh
@@ -9,22 +9,22 @@ echo Patching..
 
 echo Patching for SNEEK
 echo IOSKPatch: SD \(with di\) 
-./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP -s -d > /dev/null
+./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP.app -s -d > /dev/null
 echo elfins: Creating boot2_di.bin \(SDCard as NAND, with DI module support\)
-./ELFIns/elfins 0000000E-TMP boot2_di.bin es/esmodule.elf fs/iosmodule.elf > /dev/null
+./ELFIns/elfins 0000000E-TMP.app boot2_di.bin es/esmodule.elf fs/iosmodule.elf > /dev/null
 
 echo IOSKPatch: SD \(no di\) 
-./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP -s > /dev/null
+./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP.app -s > /dev/null
 echo elfins: Creating boot2_sd.bin \(SDCard as NAND\)
-./ELFIns/elfins 0000000E-TMP boot2_sd.bin es/esmodule.elf fs/iosmodule.elf > /dev/null
+./ELFIns/elfins 0000000E-TMP.app boot2_sd.bin es/esmodule.elf fs/iosmodule.elf > /dev/null
 
 echo Patching for UNEEK
 echo IOSKPatch: USB \(no di\)
-./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP -u > /dev/null
+./IOSKpatch/IOSKPatch 0000000e.app 0000000E-TMP.app -u > /dev/null
 echo elfins: Creating boot2_usb.bin \(USB as NAND\)
-./ELFIns/elfins 0000000E-TMP boot2_usb.bin es/esmodule.elf fs-usb/iosmodule.elf > /dev/null
+./ELFIns/elfins 0000000E-TMP.app boot2_usb.bin es/esmodule.elf fs-usb/iosmodule.elf > /dev/null
 
 echo elfins: Creating di.bin
-./ELFIns/elfins 00000001 di.bin di/iosmodule.elf > /dev/null
+./ELFIns/elfins 00000001.app di.bin di/iosmodule.elf > /dev/null
 
-rm 0000000E-TMP -v
+rm 0000000E-TMP.app -v
