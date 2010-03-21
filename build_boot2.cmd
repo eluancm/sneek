655c90861798acbaf9c6184702c0c66dfa4fea90
eggdog <eggdog@fdb4e6e6-0440-11df-9d25-5dd9327a4d76>
2010-03-21 15:44:46 +0000

    This will help folks on windows, as NUSD adds .app to the end of the decrypted files.

--- build_boot2.cmd
+++ build_boot2.cmd
@@ -10,22 +10,22 @@ echo Patching..
 
 echo Patching for SNEEK
 echo IOSKPatch: SD (with di) 
-IOSKpatch\IOSKPatch 0000000E 0000000E-TMP -s -d > NUL
+IOSKpatch\IOSKPatch 0000000E.app 0000000E-TMP.app -s -d > NUL
 echo elfins: Creating boot2_di.bin (SDCard as NAND, with DI module support)
 ELFIns\elfins 0000000E-TMP boot2_di.bin es\esmodule.elf fs\iosmodule.elf > NUL
 
 echo IOSKPatch: SD (no di) 
-IOSKpatch\IOSKPatch 0000000E 0000000E-TMP -s > NUL
+IOSKpatch\IOSKPatch 0000000E.app 0000000E-TMP.app -s > NUL
 echo elfins: Creating boot2_sd.bin (SDCard as NAND)
 ELFIns\elfins 0000000E-TMP boot2_sd.bin es\esmodule.elf fs\iosmodule.elf > NUL
 
 echo Patching for UNEEK
 echo IOSKPatch: USB (no di) 
-IOSKpatch\IOSKPatch 0000000E 0000000E-TMP -u > NUL
+IOSKpatch\IOSKPatch 0000000E.app 0000000E-TMP.app -u > NUL
 echo elfins: Creating boot2_usb.bin (USB as NAND)
-ELFIns\elfins 0000000E-TMP boot2_usb.bin es\esmodule.elf fs-usb\iosmodule.elf > NUL
+ELFIns\elfins 0000000E-TMP.app boot2_usb.bin es\esmodule.elf fs-usb\iosmodule.elf > NUL
 
 echo elfins: Creating di.bin
-ELFIns\elfins 00000001 di.bin di\iosmodule.elf > NUL
+ELFIns\elfins 00000001.app di.bin di\iosmodule.elf > NUL
 
-del 0000000E-TMP
+del 0000000E-TMP.app
