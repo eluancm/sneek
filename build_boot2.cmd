c67510903f59a75b73e51da02ba6829c00207dc5
admin <admin@det1re.de>
2010-03-22 18:18:20 +0000

    Fixed missing .app in build_boot2.cmd

--- build_boot2.cmd
+++ build_boot2.cmd
@@ -12,12 +12,12 @@ echo Patching for SNEEK
 echo IOSKPatch: SD (with di) 
 IOSKpatch\IOSKPatch 0000000e.app 0000000E-TMP.app -s -d > NUL
 echo elfins: Creating boot2_di.bin (SDCard as NAND, with DI module support)
-ELFIns\elfins 0000000E-TMP boot2_di.bin es\esmodule.elf fs\iosmodule.elf > NUL
+ELFIns\elfins 0000000E-TMP.app boot2_di.bin es\esmodule.elf fs\iosmodule.elf > NUL
 
 echo IOSKPatch: SD (no di) 
 IOSKpatch\IOSKPatch 0000000e.app 0000000E-TMP.app -s > NUL
 echo elfins: Creating boot2_sd.bin (SDCard as NAND)
-ELFIns\elfins 0000000E-TMP boot2_sd.bin es\esmodule.elf fs\iosmodule.elf > NUL
+ELFIns\elfins 0000000E-TMP.app boot2_sd.bin es\esmodule.elf fs\iosmodule.elf > NUL
 
 echo Patching for UNEEK
 echo IOSKPatch: USB (no di) 
